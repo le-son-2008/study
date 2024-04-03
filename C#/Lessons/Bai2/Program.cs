a4947577c5fd83ef199ce7f57eec878da291553b
le-son-2008 <lethankson2k8@gmail.com>
2024-04-03 16:45:04 +0700

    startcsharp

---
+++ C#/Lessons/Bai2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bai2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //khởi tạo biến
+        int soLuong = 0;
+        float diemTrungBinh = 8.5f;
+        string name = "son";
+        byte tuoi;//khai báo biến
+        System.Console.WriteLine("tên là {0} co diem trung binh la {1}",name,diemTrungBinh);
+    }
+}
