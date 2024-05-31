333f3ebe1c6e424cb6a3d1a35337561e78319559
le-son-2008 <lethankson2k8@gmail.com>
2024-05-31 04:08:24 +0100

    kotlin_2-3

---
+++ kotlin/src/bai2.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    println("kotlin-2")
+
+    //đây là 1 dòng
+//    Đây là dòng nữa
+/*    Đây là
+    một đống
+    dòng */
+
+    //xuất trên 1 dòng
+    print("kotlin-2")
+
+    //xuất trên 1 dòng rồi xuống dòng
+    println("kotlin-3")
+
+    //test \t
+    println("\t huhu")
+
+    //test \n
+    print("ngungu\nngungu")
+
+    //test "
+    println()
+    println("người xưa có câu: \"ăn quả nhớ kẻ trồng cây\"")
+}
---
+++ kotlin/src/bai3.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    //khởi tạo biến
+    var a:Byte=20
+    var b:Short=1000
+    println("giá trị của a là: "+a)
+    println("giá trị của b là: $b")
+
+    //khai báo biến
+    var c:Double
+
+    //đặt tên biến
+    var diemToan:Float
+    var diemVan:Float
+    var giáThuốc:Double
+
+}
